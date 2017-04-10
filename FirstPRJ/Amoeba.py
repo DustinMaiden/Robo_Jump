45d609ec913e4d1ed776f69b167d1a13e3d21dbe
DustinMaiden <dustinmaiden@gmail.com>
2017-04-09 23:09:59 -0500

    Saw Blade
    Add single saw blade

--- FirstPRJ/Amoeba.py
+++ FirstPRJ/Amoeba.py
@@ -5,9 +5,10 @@ from kivy.app import App
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.uix.label import Label
-from kivy.properties import ListProperty
+from kivy.uix.button import Button
 from kivy.core.text import LabelBase
 LabelBase.register(name="Robotech GP", fn_regular="ROBOTECH GP.ttf")
+
 
 class Sprite(Image):
     def __init__(self, **kwargs):
@@ -16,77 +17,93 @@ class Sprite(Image):
 
 #######################################################################################
 
+
 class Beacon(Sprite):
-    def __init__(self,pos):
+
+    def __init__(self, pos):
         super(Beacon, self).__init__(source='beacon.png', pos=pos)
 
-    #parabolic movement
-    def update(self,count2):
-        if self.y < dp(400):
+    def update(self, count2):
+        if self.y < dp(300):
             self.x -= count2
-            self.y += count2 + count2
+            self.y += count2
+
+        if self.x < dp(100):
+            self.x += count2
+
 
 class Beacon2(Sprite):
-    def __init__(self,pos):
+
+    def __init__(self, pos):
         super(Beacon2, self).__init__(source='beacon.png', pos=pos)
 
-    # parabolic movement
-    def update(self):
-         self.y += 1
-         self.x += 1
+    # simple motion
+    def update(self, count2):
+        if self.y < dp(300):
+            self.x += count2
+            self.y += count2
 
-##########################################################################################
+        if self.x > dp(600):
+            self.x -= count2
+
+###########################################################################################
+
 
 class Platform(Sprite):
-    def __init__(self,pos):
+
+    def __init__(self, pos):
         super(Platform, self).__init__(source='atlas://platform_atlas/platform_1',pos=pos)
         self.isextended = 0
+        self.play_on = 0
 
     def update(self,count):
         if count == 1:
             self.source='atlas://platform_atlas/platform_2'
         elif count == 2:
             self.source='atlas://platform_atlas/platform_3'
-            self.isextended = 1;
+            self.isextended = 1
 
-        #elif count == 3:
-         #   self.source='atlas://platform_atlas/platform_4'
-        #elif count == 4:
-         #   self.source='atlas://platform_atlas/platform_5'
+    def update2(self, count):
+        if count == 1:
+            self.source='atlas://platform_atlas/platform_4'
+        elif count == 2:
+            self.source='atlas://platform_atlas/platform_5'
+        elif count == 3:
+            self.imager2 = 1
+            self.Beacon = Beacon(pos=(dp(225), dp(0)))
+            self.parent.add_widget(self.Beacon)
+            self.Beacon2 = Beacon2(pos=(dp(425), dp(0)))
+            self.parent.add_widget(self.Beacon2)
+            self.play_on = 1
+            Clock.schedule_interval(self.update3, 1 / 10.0)
+
+    def update3(self, *ignore):
+        self.Beacon.update(self.imager2)
+        self.Beacon2.update(self.imager2)
+        self.imager2 += 1
 
     def extension(self):
         return self.isextended
 
-###########################################################################################
+    def ready_to_play(self):
+        return self.play_on
+
+#########################################################################################
 
 class Title(Sprite):
     def __init__(self,pos):
         super(Title, self).__init__(pos=pos)
-        self.roboTitle = Label(text = "Robo", font_name = 'Robotech GP', font_size = '150sp', pos = (dp(150), dp(450)))
-        self.jumpTitle = Label(text = "Jump", font_name = 'Robotech GP', font_size = '150sp', pos = (dp(550), dp(450)))
+        self.roboTitle = Label(text = "Robo  Jump", font_name = 'Robotech GP', font_size = '150sp', pos = (dp(350), dp(500)))
+        self.add_widget(self.roboTitle)
 
-    def update(self,count):
-        if count == 1:
-            self.add_widget(self.roboTitle)
-        elif count == 3:
-            self.imager2 = 1
-            self.Beacon = Beacon(pos=(225, 0))
-            self.parent.add_widget(self.Beacon)
-            Clock.schedule_interval(self.update3, 1 / 10.0)
-        elif count == 8:
-            self.add_widget(self.jumpTitle)
 
-    def update3(self, *ignore):
-        self.Beacon.update(self.imager2)
-
-        self.imager2 += 1
-
-####################################################################################
+#########################################################################################
 
 class Player(Sprite):
     def __init__(self, pos):
         super(Player, self).__init__(source='atlas://robot_atlas/base', pos=pos)
-        self.size = [75, 75]
+        self.size = [dp(75), dp(75)]
+        self.jump = 0
         self.velocity_y = 0
         self.gravity = 0
         self.ready = 0
@@ -94,32 +111,58 @@ class Player(Sprite):
     def update(self):
         self.velocity_y += self.gravity
         self.y -= self.velocity_y
-        if self.y<=180:
+
+        if self.y <= dp(180):
+            self.jump = 0
             self.velocity_y = 0
             self.gravity = 0
 
     def update2(self, count):
         if self.ready == 0 and count % 2 == 0:
             self.source = 'atlas://robot_atlas/walk_left'
-            self.y += 3
+            self.y += dp(5)
         elif self.ready == 0 and count % 2 != 0:
             self.source = 'atlas://robot_atlas/walk_right'
-            self.y += 3
+            self.y += dp(5)
 
-        if self.y >= 180:
+        if self.y >= dp(180):
             self.ready = 1
             self.source = 'atlas://robot_atlas/base'
 
     def readiness(self):
         return self.ready
 
-
     def on_touch_down(self, *ignore):
-        self.y+=125
-        self.gravity = .5
+        self.jump = 1
         self.source = 'atlas://robot_atlas/jump'
+        self.y += dp(125)
+        self.gravity = 1
 
-############################################################################
+    def is_jump(self):
+        return self.jump
+
+############################################################################################
+
+
+class Projectile(Sprite):
+    def __init__(self, pos):
+        super(Projectile, self).__init__(source='saw_blade.png', pos=pos)
+        self.size = [dp(25), dp(25)]
+        self.i = "bang"
+
+        self.velocity_x = 20
+        self.velocity_y = 0
+        self.gravity = .12
+
+    def update(self):
+        self.velocity_y += self.gravity
+        self.velocity_x = 5
+
+        self.x += self.velocity_x
+        self.y -= self.velocity_y
+
+
+#############################################################################################
 
 class Background(Widget):
     def __init__(self, source):
@@ -128,50 +171,82 @@ class Background(Widget):
         self.add_widget(self.image)
         self.size = self.image.size
 
+##############################################################################################
+
 class Game(Widget):
     def __init__(self):
         super(Game, self).__init__()
+        self.imager = 0
+        self.imager3 = 0
+        self.gameStart = 0
+
         self.background = Background(source='background.png')
         self.size = self.background.size
         self.add_widget(self.background)
 
-        self.Platform = Platform(pos=(190, 0))
-        self.add_widget(self.Platform)
-
-        self.Player = Player(pos=(330, 50))
-        self.add_widget(self.Player)
-
-        self.Title = Title(pos=(0,450))
+        self.Title = Title(pos=(dp(250),dp(450)))
         self.add_widget(self.Title)
 
-        self.imager = 0
-        self.l = "toot"
-        self.j = "bang"
-        Clock.schedule_interval(self.update, 1.0 / 60.0)
-        Clock.schedule_interval(self.update2, 1)
+        self.button1 = Button(text = 'Start Game', pos=(dp(200), dp(275)))
+        self.button1.bind(on_press=self.start)
+        self.add_widget(self.button1)
 
+        self.button2 = Button(text = 'Tutorial', pos=(dp(500), dp(275)))
+        self.add_widget(self.button2)
+        self.button2.bind(on_press=self.practice)
 
     def update(self, *ignore):
         self.Player.update()
+        self.Projectile.update()
 
+        #
+        # if self.Platform.ready_to_play() == 1:
+
+        if self.Projectile.collide_widget(self.Player) and self.Player.is_jump() == 0:
+            self.remove_widget(self.Title)
 
     def update2(self, *ignore):
         self.Platform.update(self.imager)
 
         if self.Platform.extension() == 1 and self.Player.readiness() == 0:
-            self.l = Label(text='Hello world', font_color='white', pos=(250,250))
-            self.add_widget(self.l)
             self.Player.update2(self.imager)
         elif self.Player.readiness() != 0:
-            self.j = Label(text='Goodbye world', font_color='white', pos=(250,350))
-            self.add_widget(self.j)
-
-        if self.imager < 10:
-            self.Title.update(self.imager)
+            self.Platform.update2(self.imager3)
+            self.imager3 += 1
 
         self.imager += 1
 
-#######################################################################
+
+    def start(self, pos):
+        self.button1.x = dp(25)
+        self.button1.y = dp(25)
+        self.button1.text = 'Block Left'
+
+        self.button2.x = dp(675)
+        self.button2.y = dp(25)
+        self.button2.text = 'Block Right'
+
+        self.gameStart = 1
+
+        self.Platform = Platform(pos=(dp(215), dp(0)))
+        self.add_widget(self.Platform)
+
+        self.Player = Player(pos=(dp(365), dp(50)))
+        self.add_widget(self.Player)
+
+        self.Projectile = Projectile(pos=(dp(0), dp(425)))
+        self.add_widget(self.Projectile)
+
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+        Clock.schedule_interval(self.update2, 1)
+
+    def practice(self, pos):
+       self.button2.text='tomato'
+
+    #def shoot(self):
+
+
+##############################################################################################
 
 class GameApp(App):
     def build(self):
